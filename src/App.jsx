30b3873c3efce8d3ebd5f70388f087964fcf2f5e
freelancersifat627e <freelancersifat627@gmail.comm>
2023-10-11 20:52:30 +0600

    Primary Set Up Done for starting Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/Header/Header';
 
 
 const App = () => {
   return (
     <>
-      <h1>Hello react!!</h1>
+    <Header></Header>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello react!!</h1>
     </>
   );
 };
